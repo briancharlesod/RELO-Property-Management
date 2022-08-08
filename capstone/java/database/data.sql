111d6f621d1a72337aa05c24e5ee09e7902de94e
Dan Lamb <danielkevinlamb@gmail.com>
2022-08-08 14:37:18 -0400

    rental_property inserted

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -3,4 +3,12 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO rental_property (rental_address,rental_amount,bathrooms,bedrooms,is_rented,type_of_residence) VALUES (760-782 Shady Dr, Pittsburgh, PA 15228, 1050.00, 1.0, 2, false, house);
+INSERT INTO rental_property (rental_address,rental_amount,bathrooms,bedrooms,is_rented,type_of_residence) VALUES (4925 Friendship Ave Apt 6, Pittsburgh, PA 15224, 1400.00, 1.0, 2, false, apartment);
+INSERT INTO rental_property (rental_address,rental_amount,bathrooms,bedrooms,is_rented,type_of_residence) VALUES (4925 Friendship Ave Apt 3, Pittsburgh, PA 15224, 1400.50, 1.5, 2, false, apartment);
+INSERT INTO rental_property (rental_address,rental_amount,bathrooms,bedrooms,is_rented,type_of_residence) VALUES (5737 Hobart St, Pittsburgh, PA 15217, 1648.00, 1.0, 2, false, house);
+
+
+
+
 COMMIT TRANSACTION;
